35b063cc46bbd7c3d496ea7c35291dda6747d87c
Tomás Lopes <tomas050302@gmail.com>
2020-03-21 18:31:34 +0000

    fix: breaking news slider

--- php/lib/config.php
+++ php/lib/config.php
@@ -1,5 +1,5 @@
 <?php
-$dir_site = '/opt/lampp/htdocs/noticias/';
+$dir_site = '/opt/lampp/htdocs/newswebsite/';
 $url_site = 'http://localhost.com/';
 
 require_once($dir_site . 'php/lib/functions.php');
